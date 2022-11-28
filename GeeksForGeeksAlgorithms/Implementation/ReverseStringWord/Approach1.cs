08e7563a8c140ad0f7a7c80b4f5a07fd29db4c33
Amit Kumar Pandey <amit.p@icreon.com>
2022-11-28 13:33:28 +0530

    added code for reverse string word

---
+++ GeeksForGeeksAlgorithms/Implementation/ReverseStringWord/Approach1.cs
@@ -0,0 +1,67 @@
+﻿using CommonAlgorithms.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeeksForGeeksAlgorithms.Implementation.ReverseStringWord
+{
+    public class Approach1 : IProgram
+    {
+        private readonly IInputReader inputReader;
+
+        public Approach1(IInputReader inputReader)
+        {
+            this.inputReader = inputReader;
+        }
+
+        public void Execute()
+        {
+            string input = this.inputReader.InputString();
+
+            string result = this.ReverseWord(input);
+
+            Console.WriteLine($"Reverse word is : {result}");
+        }
+
+        private string ReverseWord(string input)
+        {
+            var result = new StringBuilder();
+            var singleWord = new StringBuilder();
+            char[] chars = input.ToCharArray();
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                singleWord.Append(chars[i]);
+                if (chars[i] == ' ')
+                {
+                    result.Append(this.ReverseString(singleWord.ToString()));
+                    singleWord.Clear();
+                }
+                else if (i == 0)
+                {
+                    result.Append($" {this.ReverseString(singleWord.ToString())}");
+                    singleWord.Clear();
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private string ReverseString(string value)
+        {
+            char[] chars = value.ToCharArray();
+            char temp;
+            int start = 0;
+            int end = chars.Length - 1;
+            while (start <= end)
+            {
+                temp = chars[start];
+                chars[start] = chars[end];
+                chars[end] = temp;
+                start++;
+                end--;
+            }
+
+            return new string(chars);
+        }
+    }
+}
--- GeeksForGeeksClient/Program.cs
+++ GeeksForGeeksClient/Program.cs
@@ -7,6 +7,10 @@ using SortArrayStringsByDictionaryOrder = GeeksForGeeksAlgorithms.Implementation
 using SortStringOfChars = GeeksForGeeksAlgorithms.Implementation.SortStringOfChars;
 using AlternateUpperLowerStringSort = GeeksForGeeksAlgorithms.Implementation.AlternateUpperLowerStringSort;
 using SparseSearch = GeeksForGeeksAlgorithms.Implementation.SparseSearch;
+using ReverseStringWord = GeeksForGeeksAlgorithms.Implementation.ReverseStringWord;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace GeeksForGeeksClient
 {
@@ -14,24 +18,30 @@ namespace GeeksForGeeksClient
     {
         static void Main(string[] args)
         {
+            //RemoveHtmlTags();
+            //return;
             IInputReader inputReader = new InputReader();
             //IProgram program = new PermutationOfAString.Approach1(inputReader);
             //IProgram program = new SortArrayStringsAccordingToLength.Approach1(inputReader);
             //IProgram program = new SortArrayStringsByDictionaryOrder.Approach1(inputReader);
             //IProgram program = new SortStringOfChars.Approach1(inputReader);
             //IProgram program = new SparseSearch.Approach1(inputReader);
+            IProgram program = new ReverseStringWord.Approach1(inputReader);
 
-            //program.Execute();
+            program.Execute();
 
             //int[] values = inputReader.ArrayIntegerReader();
             //SortInteger(values);
 
-            string input = inputReader.SpaceSeparatedString();
-            SortWord(input.Split(' '));
-            
+            //string input = inputReader.SpaceSeparatedString();
+            //SortWord(input.Split(' '));
+
+            //ModifyTheListData();
+
+            PrintTheFormat(4);
+
             Console.Read();
         }
-
 
         static void SortInteger(int[] values)
         {
@@ -77,6 +87,72 @@ namespace GeeksForGeeksClient
             {
                 Console.WriteLine(item);
             }
+        }
+
+        static void ModifyTheListData()
+        {
+            var templateParameteres = new List<EmailTemplateParameters>();
+
+            var parameter = new EmailTemplateParameters
+            {
+                MemberFirstName = "Fname",
+                MemberLastName = "Lname",
+                ClassificationTypeName = "User",
+                ClassificationUpdatedDate = "20-05-2022",
+                CommitteeDesignation = "A23"
+            };
+
+            templateParameteres.Add(parameter);
+
+            TemplateParameterHelper.SetClassificationTypeNamePropertyToProducer(templateParameteres);
+
+            //templateParameteres.Select(s => { s.ClassificationTypeName = "Producer"; return s; }).ToList();
+        }
+
+        static void PrintTheFormat(int input)
+        {
+            int counter = 1;
+
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"{counter} ");
+                    counter++;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void RemoveHtmlTags()
+        {
+            string html = "subgroup average (<img src=\" / HTTP / IMAGES / E2587 - 16R21E01_8.gif\">), n";
+            Regex HtmlRegex = new Regex("<.*?>", RegexOptions.Compiled);
+            string value = HtmlRegex.Replace(html, string.Empty);
+        }
+    }
+
+    public class EmailTemplateParameters
+    {
+        public string MemberFirstName { get; set; }
+
+        public string MemberLastName { get; set; }
+
+        public string ClassificationUpdatedDate { get; set; }
+
+        public string CommitteeDesignation { get; set; }
+
+        public string ClassificationTypeName { get; set; }
+    }
+
+    public static class TemplateParameterHelper
+    {
+        public static void SetClassificationTypeNamePropertyToProducer(List<EmailTemplateParameters> templateParameteres)
+        {
+            templateParameteres
+                .Select(s => { s.ClassificationTypeName = "Producer"; return s; })
+                .ToList();
         }
     }
 }
